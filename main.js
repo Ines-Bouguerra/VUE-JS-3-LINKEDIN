6861aca8189506ce16ea22f4242a7ef788b167b0
Ines BOUGUERRA <ines.bouguerra@medianet.lan>
2022-01-30 13:07:15 +0100

    Interpolatin & attribute binding

---
+++ main.js
@@ -0,0 +1,9 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            name: 'Pizza Royale',
+            price:12,
+            image: 'assets/images/pizza1-tomate.jpg'
+        }
+    }
+})
