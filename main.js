77541c27fe4f3afedeff50913aa12f5be649997e
Ines BOUGUERRA <ines.bouguerra@medianet.lan>
2022-01-30 13:34:43 +0100

    use loop with vue js

--- main.js
+++ main.js
@@ -5,7 +5,40 @@ const app=Vue.createApp({
             price:12,
             image: 'assets/images/pizza1-tomate.jpg',
             sale:true,
-            notAvailable:false
+            notAvailable:false,
+            ingredients: [
+                "Olives",
+                "Poulet roti",
+                "Bacon",
+                "Poivrons",
+                "Champignons",
+                "Mozzarella",
+                "Oeuf"
+              ],
+            sauces: [
+                {
+                  id: 1001,
+                  type: "Sauce Tomate",
+                  color: "#db4006",
+                  image: require("../assets/images/pizza1-tomate.jpg")
+                },
+                {
+                  id: 1002,
+                  type: "Crème Fraiche",
+                  color: "#e9cb8f",
+                  image: require("../assets/images/pizza1-creme.jpg")
+                }
+              ],
+              energy: {
+                Kcal: 242,
+                Glucides: 27.99,
+                Fibres: 1.75,
+                Proteines: 9.62,
+                Sel: 11
+              },
+            
+            
+            
         }
     }
 })
